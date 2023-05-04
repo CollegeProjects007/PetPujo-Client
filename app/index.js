8e3eefd2483b95607e2a2c343b985da56552c283
sp-xd <69642436+SP-XD@users.noreply.github.com>
2023-05-05 04:15:43 +0530

    feat: add custom font

--- app/index.js
+++ app/index.js
@@ -1,119 +1,140 @@
 import * as React from "react";
-import { View, Text, Image } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { useCallback } from "react";
+import { Image } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
 import AccountScreen from "./screens/AccountScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function App() {
+  const [fontsLoaded] = useFonts({
+    Sen: require("../assets/fonts/sen-regular.otf"),
+    "Sen-Bold": require("../assets/fonts/sen-bold.otf"),
+    "Sen-ExtraBold": require("../assets/fonts/sen-extrabold.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-      <Stack.Navigator
-        initialRouteName="BottomTabNav"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="BottomTabNav" component={BottomTabNav} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="BottomTabNav"
+      screenOptions={{ headerShown: false }}
+      onLayoutRootView={onLayoutRootView}
+    >
+      <Stack.Screen name="BottomTabNav" component={BottomTabNav} />
+      <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+    </Stack.Navigator>
   );
 }
 
 function BottomTabNav() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: "#000",
-          opacity: 0.9,
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          height: 100,
-          borderTopRightRadius: 25,
-          borderTopLeftRadius: 25,
-          paddingTop: 15,
-          paddingBottom: 15,
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Explore"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: "Explore",
-          tabBarActiveTintColor: "#fff",
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "bold",
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#000",
+            opacity: 0.9,
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 100,
+            borderTopRightRadius: 25,
+            borderTopLeftRadius: 25,
+            paddingTop: 15,
+            paddingBottom: 15,
           },
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/home_icon.png")}
-              resizeMode="contain"
-              style={{
-                width: 30,
-                height: 30,
-                tintColor: focused ? "#fff" : "gray",
-              }}
-            />
-          ),
         }}
-      />
-      <Tab.Screen
-        name="Cart"
-        component={CartScreen}
-        options={{
-          tabBarLabel: "  Cart",
-          tabBarActiveTintColor: "#fff",
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/cart_icon.png")}
-              resizeMode="contain"
-              style={{
-                width: 35,
-                height: 35,
-                tintColor: focused ? "#fff" : "gray",
-              }}
-            />
-          ),
-        }}
-      />
+      >
+        <Tab.Screen
+          name="Explore"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Explore",
+            tabBarActiveTintColor: "#fff",
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: "bold",
+            },
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/icons/home_icon.png")}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? "#fff" : "gray",
+                }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarLabel: "  Cart",
+            tabBarActiveTintColor: "#fff",
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: "bold",
+            },
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/icons/cart_icon.png")}
+                resizeMode="contain"
+                style={{
+                  width: 35,
+                  height: 35,
+                  tintColor: focused ? "#fff" : "gray",
+                }}
+              />
+            ),
+          }}
+        />
 
-      <Tab.Screen
-        name="Account"
-        component={AccountScreen}
-        options={{
-          tabBarLabel: "Account",
-          tabBarActiveTintColor: "#fff",
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/profile_icon.png")}
-              resizeMode="contain"
-              style={{
-                width: 30,
-                height: 30,
-                tintColor: focused ? "#fff" : "gray",
-              }}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarLabel: "Account",
+            tabBarActiveTintColor: "#fff",
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: "bold",
+            },
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/icons/profile_icon.png")}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? "#fff" : "gray",
+                }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
   );
 }
 
