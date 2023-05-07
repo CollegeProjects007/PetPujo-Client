564efe4a07ddd88fad2062f5e68ffef9ea02a129
sp-xd <69642436+SP-XD@users.noreply.github.com>
2023-05-08 03:49:41 +0530

    feat: account and orderplaced screen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,16 +1,26 @@
 import { Link } from "expo-router";
 import { View, Text } from "react-native";
+import { Button, NativeBaseProvider } from "native-base";
 
 export default function RestaurantPage() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Account Page</Text>
-    </View>
+    <NativeBaseProvider>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 15,
+          gap: 10,
+          height: "100%",
+          width: "100%",
+          backgroundColor: "orange",
+        }}
+      >
+        <Text fontSize={"6xl"}></Text>
+        <Text fontSize={"3xl"} color={"black"}>
+          Account in Progress !!
+        </Text>
+      </View>
+    </NativeBaseProvider>
   );
 }
---
+++ app/screens/OrderplacedScreen.js
@@ -0,0 +1,57 @@
+import { View } from "react-native";
+import {
+  ZStack,
+  Box,
+  Image,
+  NativeBaseProvider,
+  HStack,
+  VStack,
+  Text,
+  ScrollView,
+  Button,
+} from "native-base";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function OrderPlacedScreen({ navigation }) {
+  return (
+    <NativeBaseProvider>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 15,
+          gap: 10,
+          height: "100%",
+          width: "100%",
+          backgroundColor: "orange",
+        }}
+      >
+        <Text fontSize={"6xl"}>🍲</Text>
+        <Text fontSize={"3xl"} color={"black"}>
+          Order Placed Successfully !!
+        </Text>
+        <Button
+          padding={3}
+          width={"100%"}
+          bg={"orange.500"}
+          rounded={"xl"}
+          onPress={() => {
+            console.log("Checkout");
+            navigation.push("OrderPlaced");
+          }}
+        >
+          <Text
+            color={"white"}
+            fontSize={"lg"}
+            onPress={() => {
+              navigation.navigate("Explore");
+            }}
+          >
+            Go to Home
+          </Text>
+        </Button>
+      </View>
+    </NativeBaseProvider>
+  );
+}
