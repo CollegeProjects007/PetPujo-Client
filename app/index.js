214a083657d5e0837fb4e1fa2f17b03a06710dbe
sp-xd <69642436+SP-XD@users.noreply.github.com>
2023-05-08 00:57:24 +0530

    feat: add mapscreen to route

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
 import AccountScreen from "./screens/AccountScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import MapScreen from "./screens/MapScreen";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -40,101 +41,102 @@ function App() {
     >
       <Stack.Screen name="BottomTabNav" component={BottomTabNav} />
       <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+      <Stack.Screen name="Map" component={MapScreen} />
     </Stack.Navigator>
   );
 }
 
 function BottomTabNav() {
   return (
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: "#000",
-            opacity: 0.9,
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            right: 0,
-            height: 90,
-            borderTopRightRadius: 25,
-            borderTopLeftRadius: 25,
-            paddingTop: 15,
-            paddingBottom: 15,
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#000",
+          opacity: 0.9,
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: 90,
+          borderTopRightRadius: 25,
+          borderTopLeftRadius: 25,
+          paddingTop: 15,
+          paddingBottom: 15,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Explore"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Explore",
+          tabBarActiveTintColor: "#fff",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold",
           },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/icons/home_icon.png")}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "#fff" : "gray",
+              }}
+            />
+          ),
         }}
-      >
-        <Tab.Screen
-          name="Explore"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: "Explore",
-            tabBarActiveTintColor: "#fff",
-            tabBarLabelStyle: {
-              fontSize: 12,
-              fontWeight: "bold",
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={require("../assets/icons/home_icon.png")}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? "#fff" : "gray",
-                }}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={CartScreen}
-          options={{
-            tabBarLabel: "  Cart",
-            tabBarActiveTintColor: "#fff",
-            tabBarLabelStyle: {
-              fontSize: 12,
-              fontWeight: "bold",
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={require("../assets/icons/cart_icon.png")}
-                resizeMode="contain"
-                style={{
-                  width: 30,
-                  height: 30,
-                  tintColor: focused ? "#fff" : "gray",
-                }}
-              />
-            ),
-          }}
-        />
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarLabel: "  Cart",
+          tabBarActiveTintColor: "#fff",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold",
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/icons/cart_icon.png")}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+                tintColor: focused ? "#fff" : "gray",
+              }}
+            />
+          ),
+        }}
+      />
 
-        <Tab.Screen
-          name="Account"
-          component={AccountScreen}
-          options={{
-            tabBarLabel: "Account",
-            tabBarActiveTintColor: "#fff",
-            tabBarLabelStyle: {
-              fontSize: 12,
-              fontWeight: "bold",
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={require("../assets/icons/profile_icon.png")}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? "#fff" : "gray",
-                }}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: "Account",
+          tabBarActiveTintColor: "#fff",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold",
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/icons/profile_icon.png")}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "#fff" : "gray",
+              }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
